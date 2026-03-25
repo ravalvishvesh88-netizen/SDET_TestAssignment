24de5107eefbcfc48ceae97e59cb4b9b78979277
Vishvesh <raval.vishvesh88@gmail.com>
2026-03-25 18:11:22 +0530

    Fixed Logs folder issue

--- TestAssignment/Tests/RegisterTests.cs
+++ TestAssignment/Tests/RegisterTests.cs
@@ -5,9 +5,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAssignment.Logs;
 using TestAssignment.Pages;
 using TestAssignment.Tests;
-using TestAssignment.Logs;
 
 
 namespace TestAssignment.Tests
